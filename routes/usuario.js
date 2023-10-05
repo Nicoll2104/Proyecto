d88fb13fd6df4dd5c652b790294db912d33fec15
Nicoll2104 <soda210461@gmail.com>
2023-10-05 17:00:36 -0500

    primero

--- routes/usuario.js
+++ routes/usuario.js
@@ -7,18 +7,46 @@ const router = Router();
 
 router.get("/ver", httpUsuario.getUsuario);
 
-router.get("/usuario/:id", httpUsuario.getUsuarioid);
+router.get("/usuario/:id",[
+    check("id", "El id es obligatorio").not().isEmpty(),
+    validarcampos
+], httpUsuario.getUsuarioid);
 
-router.post("/agregar", httpUsuario.postUsuario);
+router.post("/agregar",[
+    check("nombre","El nombre es obligatorio").not().isEmpty(),
+    check("cedula", "La cedula es obligatoria").not().isEmpty(),
+    check('correo', 'El correo tiene que llevar @ y es obligatorio').isEmail().notEmpty(),
+    check("telefono", "El telefono es obligatorio").not().isEmpty(),
+    check("contrasena", "La contrasena es obligatoria").not().isEmpty(),
+    check("rol","El rol es obligatorio").not().isEmpty(),
+    validarcampos
+], httpUsuario.postUsuario);
 
 router.post("/login", httpUsuario.login)
 
-router.put("/modificar/:id", httpUsuario.putUsuario);
+router.put("/modificar/:id",[
+    check("nombre","El nombre es obligatorio").not().isEmpty(),
+    check("cedula", "La cedula es obligatoria").not().isEmpty(),
+    check('correo', 'El correo tiene que llevar @ y es obligatorio').isEmail().notEmpty(),
+    check("telefono", "El telefono es obligatorio").not().isEmpty(),
+    check("contrasena", "La contrasena es obligatoria").not().isEmpty(),
+    check("rol","El rol es obligatorio").not().isEmpty(),
+    validarcampos
+],httpUsuario.putUsuario);
 
-router.put("/inactivar/:id", httpUsuario.putInactivar);
+router.put("/inactivar/:id",[
+    check("id", "Digite ID").not().isEmpty(),
+    validarcampos
+], httpUsuario.putInactivar);
 
-router.put("activar/:id", httpUsuario.putActivar);
+router.put("activar/:id",[
+    check("id", "Digite ID").not().isEmpty(),
+    validarcampos
+], httpUsuario.putActivar);
 
-router.delete("/eliminar/:id", httpUsuario.deleteUsuario)
+router.delete("/eliminar/:id",[
+    check("id", "Digite ID").not().isEmpty(),
+    validarcampos
+], httpUsuario.deleteUsuario)
 
 export default router;
