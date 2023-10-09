cc23e21549efc2b855515c348c1d0a97a1724946
Karol <124207015+Karollll@users.noreply.github.com>
2023-10-09 17:55:49 -0500

    hola

--- models/pedido.js
+++ models/pedido.js
@@ -6,9 +6,12 @@ const pedido = new mongoose.Schema(
         fecha_entrega:{type:Date, required: true,},
         /* id_distribucion_lote_ficha:{type:mongoose.Schema.Types.ObjectId,ref:'distribucion_lote_ficha', require:true}, */
         instructor_encargado:{type:String, required: true,},
+        subtotal:{type:Number, required: true,},
+        total:{type:Number, required: true,},
+        estado_solicitud:{type:String, required: true,},
         ficha_id:{type:mongoose.Schema.Types.ObjectId,ref:'ficha', require:true},
         createdAt: { type: Date, default: Date.now },
         status:{type:String,default:1}
     })
 
-    export default mongoose.model("Area", area)
+    export default mongoose.model("Pedido", pedido)
