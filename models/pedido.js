3e6adaa59026966ae29a111e38b2686b3c765fe0
lauraRodrigue <>
2023-10-19 18:04:54 -0500

    laura

--- models/pedido.js
+++ models/pedido.js
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 
 const pedido = new mongoose.Schema(
     {
-        fecha_creacion:{type:Date, required: true,},
-        fecha_entrega:{type:Date, required: true,},
-        /* id_distribucion_lote_ficha:{type:mongoose.Schema.Types.ObjectId,ref:'distribucion_lote_ficha', require:true}, */
-        instructor_encargado:{type:String, required: true,},
-        subtotal:{type:Number, required: true,},
-        total:{type:Number, required: true,},
-        estado_solicitud:{type:String, required: true,},
-        ficha_id:{type:mongoose.Schema.Types.ObjectId,ref:'ficha', require:true},
+        fecha_creacion:{type:Date, required: true},
+        fecha_entrega:{type:Date, required: true},
+        instructor_encargado:{type:String, required: true},
+        subtotal:{type:Number, required: true},
+        total:{type:Number, required: true},
+        impuestos:{type:Number, required: true},
+        distribucion_ficha:{type:mongoose.Schema.Types.ObjectId,ref:'distribucion_ficha', require:true},
+        uruario:{type:mongoose.Schema.Types.ObjectId,ref:'usuario', require:true},
         createdAt: { type: Date, default: Date.now },
         status:{type:String,default:1}
     })
 
-    export default mongoose.model("Pedido", pedido)
+ export default mongoose.model("Pedido", pedido)
