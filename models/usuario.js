12eba26ecf915c7e65bd4267ae20a5ad0fbe1323
Nicoll2104 <soda210461@gmail.com>
2024-01-25 17:47:34 -0500

    actual

--- models/usuario.js
+++ models/usuario.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const usuario = new mongoose.Schema(
     {
         nombre:{type:String, required: true,},
-        cedula:{type:String, required: true, unique:true,},
+        cedula:{type:String, required: true, unique:true,minlength:7,maxlength:10},
         correo:{type:String, required: true,},
-        telefono:{type:String, required:true,minlength:10, maxlength:10},
+        telefono:{type:String, required:true,minlength:9},
         contrasena:{type:String, required:true,minlength:8},
         rol:{type:String, required:true},
         createdAt: { type: Date, default: Date.now },
