38fc9eb162f0f4fc3660c39b549cb5d7df94cde4
Karol <124207015+Karollll@users.noreply.github.com>
2024-01-31 16:13:13 -0500

    holis

--- models/producto.js
+++ models/producto.js
@@ -4,9 +4,9 @@ const producto = new mongoose.Schema(
     {
         codigo:{type:String, required: true, unique:true,},
         nombre:{type:String, required: true,},
-        descripcion:{type:String, required: true},
+        descripcion:{type:String, required: true,},
         unidad_medida:{type:String, required:true,},
-        precio_unitario:{type:Number, required:true},
+        precio_unitario:{type:Number, required:true,},
         impestos:{type:Number, required:true},
         fecha_creacion:{type:Date, required:true},
         fecha_vencimiento:{type:Date, required:true},
