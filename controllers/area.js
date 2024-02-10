3021ff5c91cefff33af0cff62bcf9fdbea1ab819
Nicoll2104 <you@example.com>
2024-02-09 21:32:21 -0500

    acyual

--- controllers/area.js
+++ controllers/area.js
@@ -3,45 +3,45 @@ import area from "../models/area.js";
 const httpArea ={
     getArea: async (req, res) => {
         try {
-            const areas = await area.find()
-            res.json({ areas })
+            const areas = await area.find().populate('ficha_id');
+            res.json({ areas });
 
         } catch (error) {
-            res.status(400).json({ error:'Error interno del servidor' })
+            res.status(400).json({ error: 'Error interno del servidor' });
         }
     },
     getAreaId: async (req, res) => {
-        const { id } = req.params
+        const { id } = req.params;
         try {
-            const areas = await area.findById(id)
-            res.json({ areas })
+            const areas = await area.findById(id).populate('ficha_id');
+            res.json({ areas });
 
         } catch (error) {
-            res.status(400).json({ error: 'Error interno del servidor'  })
+            res.status(400).json({ error: 'Error interno del servidor' });
         }
     },
 
     postArea: async (req, res) => {
         try {
-            const { nombre, presupuesto, ficha_id} = req.body
-            const areas = new area({ nombre, presupuesto, ficha_id })
-            await areas.save()
+            const { nombre, presupuesto, ficha_id } = req.body;
+            const areas = new area({ nombre, presupuesto, ficha_id });
+            await areas.save();
 
-            res.json({ mensaje: 'Area agregada exitosamente' })
+            res.json({ mensaje: 'Area agregada exitosamente' });
         } catch (error) {
-            res.status(400).json({ error: 'Error interno del servidor' })
+            res.status(400).json({ error: 'Error interno del servidor' });
         }
     },
 
     putArea: async (req,res) =>{
-        const {id} = req.params;
-        const {nombre, presupuesto, ficha_id} = req.body;
+        const { id } = req.params;
+        const { nombre, presupuesto, ficha_id } = req.body;
 
         try{
-            const areas  = await area.findByIdAndUpdate(id, { nombre, presupuesto, ficha_id }, { new: true });
+            const areas  = await area.findByIdAndUpdate(id, { nombre, presupuesto, ficha_id }, { new: true }).populate('ficha_id');
 
             if(!areas){
-                return res.status(404).json({mensaje: 'El area no existe' })
+                return res.status(404).json({ mensaje: 'El area no existe' });
             }
             res.json({ mensaje: 'Area actualizado con éxito', areas });
         }catch(error){
@@ -51,31 +51,30 @@ const httpArea ={
 
     deleteArea: async(req,res)=>{
         try {
-            const {id}=req.params
-            const areas= await area.findByIdAndRemove(id)
-            res.json({areas})
+            const { id } = req.params;
+            const areas = await area.findByIdAndRemove(id);
+            res.json({ areas });
         } catch (error) {
-            res.status(400).json({error: 'Se produjo un error'})
+            res.status(400).json({ error: 'Se produjo un error' });
         }
     },
     
     putInactivar: async (req,res)=>{
         try {
-            const {id}=req.params
-            const areas=await area.findByIdAndUpdate(id,{status:0},{new:true})
-            res.json({areas})
+            const { id } = req.params;
+            const areas = await area.findByIdAndUpdate(id, { status: 0 }, { new: true }).populate('ficha_id');
+            res.json({ areas });
         } catch (error) {
-            res.status(400).json({error: 'Se produjo un error'})
-            
+            res.status(400).json({ error: 'Se produjo un error' });   
         }
     },
     putActivar: async (req,res)=>{
         try {
-            const {id}=req.params
-            const areas=await area.findByIdAndUpdate(id,{status:1},{new:true})
-            res.json({areas})
+            const { id } = req.params;
+            const areas = await area.findByIdAndUpdate(id, { status: 1 }, { new: true }).populate('ficha_id');
+            res.json({ areas });
         } catch (error) {
-            res.status(400).json({error: 'Se produjo un error'})
+            res.status(400).json({ error: 'Se produjo un error' });
         }
     }
 }
