6a02c9b8763163c6e923ec0bacdd3a539dbdfcc6
Nicoll2104 <you@example.com>
2024-02-13 18:25:06 -0500

    actual

--- controllers/ficha.js
+++ controllers/ficha.js
@@ -23,53 +23,31 @@ const httpFicha = {
     postFicha: async (req, res) => {
         try {
             const { codigo_ficha, nombre, nivel_de_formacion, fecha_inicio, fecha_fin, area } = req.body;
-            
-            // Verificar si los datos enviados son válidos antes de crear un nuevo documento
-            if (!codigo_ficha || !nombre || !nivel_de_formacion || !fecha_inicio || !fecha_fin || !area) {
-                return res.status(400).json({ error: 'Faltan campos obligatorios' });
-            }
-            
             const fichas = new ficha({ codigo_ficha, nombre, nivel_de_formacion, fecha_inicio, fecha_fin, area });
-            await fichas.save(); // Guardar el nuevo documento
-            
+            await fichas.populate('area').execPopulate(); 
+            await fichas.save(); 
             res.json({ mensaje: 'Ficha agregada con éxito', fichas });
         } catch (error) {
-            // Capturar cualquier error y devolver una respuesta adecuada
-            console.error(error); // Registrar el error en la consola para depuración
             res.status(500).json({ error: 'Error interno del servidor' });
         }
     },
     
-    putFicha: async (req, res) => {
-        const { id } = req.params;
-        const { codigo_ficha, nombre, nivel_de_formacion, fecha_inicio, fecha_fin, area } = req.body;
     
-        try {
-            // Verificar si el ID proporcionado es válido
-            if (!mongoose.Types.ObjectId.isValid(id)) {
-                return res.status(400).json({ error: 'ID de ficha no válido' });
+    putFicha: async (req,res) =>{
+        const {id} = req.params;
+        const {codigo_ficha, nombre,nivel_de_formacion, fecha_inicio, fecha_fin,area} = req.body;
+    
+        try{
+            const fichas  = await ficha.findByIdAndUpdate(id, { codigo_ficha, nombre,nivel_de_formacion, fecha_inicio, fecha_fin,area }, { new: true }).populate('area');
+
+            if(!fichas){
+                return res.status(404).json({mensaje: 'La ficha no existe' })
             }
-    
-            // Verificar si los datos enviados son válidos antes de actualizar el documento
-            if (!codigo_ficha || !nombre || !nivel_de_formacion || !fecha_inicio || !fecha_fin || !area) {
-                return res.status(400).json({ error: 'Faltan campos obligatorios' });
-            }
-    
-            // Actualizar el documento con los datos proporcionados
-            const fichas = await ficha.findByIdAndUpdate(id, { codigo_ficha, nombre, nivel_de_formacion, fecha_inicio, fecha_fin, area }, { new: true });
-    
-            if (!fichas) {
-                return res.status(404).json({ mensaje: 'La ficha no existe' });
-            }
-    
-            res.json({ mensaje: 'Ficha actualizada con éxito', fichas });
-        } catch (error) {
-            // Capturar cualquier error y devolver una respuesta adecuada
-            console.error(error); // Registrar el error en la consola para depuración
+            res.json({ mensaje: 'Ficha actualizado con éxito', fichas });
+        }catch(error){
             res.status(500).json({ error: 'Error interno del servidor' });
         }
     },
-    
 
     deleteFicha: async (req,res) =>{
         try{
