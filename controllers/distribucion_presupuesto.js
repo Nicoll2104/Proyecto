4feb22a9dea5264d032904ec487fc6def0b850ea
Nicoll2104 <soda210461@gmail.com>
2024-02-14 17:29:50 -0500

    actual

--- controllers/distribucion_presupuesto.js
+++ controllers/distribucion_presupuesto.js
@@ -1,9 +1,11 @@
 import distribucion_presupuesto from "../models/distribucion_presupuesto.js";
+import Lote from "../models/lote.js";
+import Items from "../models/items_presupuesto.js";
 
 const httpDistribucionPresupesto ={
     getDisPresupuesto: async (req, res) => {
         try {
-            const distribucion = await distribucion_presupuesto.find()
+            const distribucion = await distribucion_presupuesto.find().populate('lote').populate('items')
             res.json({ distribucion })
 
         } catch (error) {
@@ -13,7 +15,7 @@ const httpDistribucionPresupesto ={
     getDisPresupuestoId: async (req, res) => {
         const { id } = req.params
         try {
-            const distribucion = await distribucion_presupuesto.findById(id)
+            const distribucion = await distribucion_presupuesto.findById(id).populate('lote').populate('items')
             res.json({ distribucion})
 
         } catch (error) {
@@ -25,6 +27,12 @@ const httpDistribucionPresupesto ={
         try {
             const { codigo_presupuestal,nombre,presupuesto_inicial,ano,lote,items } = req.body
             const distribucion = new distribucion_presupuesto({ codigo_presupuestal,nombre,presupuesto_inicial,ano,lote,items })
+            
+            const rLote = await Lote.findById(lote)
+            const rItems = await Items.findById(items)
+
+            distribucion.lote = rLote
+            distribucion.items = rItems
             await distribucion.save()
 
             res.json({ mensaje: 'Distribucion del presupuesto agregada exitosamente', distribucion })
@@ -43,6 +51,14 @@ const httpDistribucionPresupesto ={
             if(!distribucion){
                 return res.status(404).json({mensaje: 'La distribucion del presupuesto no existe' })
             }
+
+            const rLote = await Lote.findById(lote)
+            const rItems = await Items.findById(items)
+
+            distribucion.lote = rLote
+            distribucion.items = rItems
+            await distribucion.save()
+
             res.json({ mensaje: 'Distribucion del presupuesto actualizado con éxito', distribucion });
         }catch(error){
             res.status(500).json({ error: 'Error interno del servidor' });
