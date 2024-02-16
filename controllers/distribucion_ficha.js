12755bd3a3d638972cd721b99aa4ba68e2cfec74
sairarojas <131273447+YesidRojas06@users.noreply.github.com>
2024-02-16 14:26:39 -0500

    holis

--- controllers/distribucion_ficha.js
+++ controllers/distribucion_ficha.js
@@ -1,69 +1,65 @@
-import distribucion_ficha from "../models/distribucion_ficha.js";
-import Ficha from "../models/ficha.js";
-import Distribucion_presupuesto from "../models/distribucion_presupuesto.js";;
+const distribucion_ficha = require('../models/distribucion_ficha');
+const Distribucion_presupuesto = require('../models/distribucion_presupuesto');
+const Ficha = require('../models/ficha');
 
-const httpDistribucionFicha ={
-    getDisFicha: async (req, res) => {
+const httpDistribucionFicha = {
+    getDisFichas: async (req, res) => {
         try {
-            const distribucion = await distribucion_ficha.find().populate('distribucion_presupuesto').populate('ficha');
-            res.json({ distribucion })
-
+            const distribuciones = await distribucion_ficha.find().populate('distribucion_presupuesto').populate('ficha');
+            res.json({ distribuciones });
         } catch (error) {
-            res.status(400).json({ error:'Error interno del servidor' })
+            res.status(400).json({ error: 'Error interno del servidor' });
         }
     },
     getDisFichaId: async (req, res) => {
-        const { id } = req.params
+        const { id } = req.params;
         try {
             const distribucion = await distribucion_ficha.findById(id).populate('distribucion_presupuesto').populate('ficha');
-            res.json({ distribucion})
-
+            res.json({ distribucion });
         } catch (error) {
-            res.status(400).json({ error: 'Error interno del servidor'  })
+            res.status(400).json({ error: 'Error interno del servidor' });
         }
     },
-
     postDisFicha: async (req, res) => {
         try {
-            const { presupuesto, distribucion_presupuesto, ficha } = req.body
-            const distribucion = new distribucion_ficha({ presupuesto, distribucion_presupuesto, ficha })
-           
-            const disPres = await Distribucion_presupuesto.findById(distribucion_presupuesto)
-            const dfichas = await Ficha.findById(ficha)
+            const { presupuesto, distribucion_presupuesto, ficha } = req.body;
+            const distribucion = new distribucion_ficha({ presupuesto, distribucion_presupuesto, ficha });
 
-            distribucion.distribucion_presupuesto = disPres
-            distribucion.ficha = dfichas
+            const disPres = await Distribucion_presupuesto.findById(distribucion_presupuesto);
+            const dfichas = await Ficha.findById(ficha);
 
-            await distribucion.save()
+            distribucion.distribucion_presupuesto = disPres;
+            distribucion.ficha = dfichas;
 
-            res.json({ mensaje: 'Distribucion de la ficha agregada exitosamente' , distribucion })
+            await distribucion.save();
+
+            res.json({ mensaje: 'Distribucion de la ficha agregada exitosamente', distribucion });
         } catch (error) {
-            res.status(400).json({ error: 'Error interno del servidor' })
+            res.status(400).json({ error: 'Error interno del servidor' });
         }
     },
-
-    putDisFicha: async (req,res) =>{
-        const {id} = req.params;
+    putDisFicha: async (req, res) => {
+        const { id } = req.params;
         const { presupuesto, distribucion_presupuesto, ficha } = req.body;
 
-        try{
-            const distribucion  = await distribucion_ficha.findByIdAndUpdate(id, { presupuesto, distribucion_presupuesto, ficha }, { new: true });
+        try {
+            const distribucion = await distribucion_ficha.findByIdAndUpdate(id, { presupuesto, distribucion_presupuesto, ficha }, { new: true });
 
-            if(!distribucion){
-                return res.status(404).json({mensaje: 'La distribucion de la ficha no existe' })
+            if (!distribucion) {
+                return res.status(404).json({ mensaje: 'La distribucion de la ficha no existe' });
             }
-            const disPres = await Distribucion_presupuesto.findById(distribucion_presupuesto)
-            const dfichas = await Ficha.findById(ficha)
 
-            distribucion.distribucion_presupuesto = disPres
-            distribucion.ficha = dfichas
+            const disPres = await Distribucion_presupuesto.findById(distribucion_presupuesto);
+            const dfichas = await Ficha.findById(ficha);
+
+            distribucion.distribucion_presupuesto = disPres;
+            distribucion.ficha = dfichas;
 
             res.json({ mensaje: 'Distribucion de la ficha actualizado con éxito', distribucion });
-        }catch(error){
+        } catch (error) {
             res.status(500).json({ error: 'Error interno del servidor' });
         }
     },
-
     deleteDisFicha: async(req,res)=>{
         try {
             const {id}=req.params
