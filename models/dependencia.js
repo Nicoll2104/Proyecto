e11a7c07046f0629da4b7defbfdeb83a0a82c3c1
Nicoll2104 <soda210461@gmail.com>
2024-03-21 13:27:38 -0500

    first commit

--- models/dependencia.js
+++ models/dependencia.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const dependecia = new mongoose.Schema(
+const dependencia = new mongoose.Schema(
     {
         codigo:{type:String, required: true,},
         nombre:{type:String, required: true,},
@@ -9,4 +9,4 @@ const dependecia = new mongoose.Schema(
         status:{type:String,default:1}
     })
 
-    export default mongoose.model("Dependencia", dependecia)
+    export default mongoose.model("Dependencia", dependencia)
