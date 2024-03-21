0af665ed425e0ac0f626cf8c1dc820435f29bcd9
lauraRodrigue <>
2024-03-21 13:24:18 -0500

    distribucion

--- models/Dis_dependencia.js
+++ models/Dis_dependencia.js
@@ -5,7 +5,7 @@ const distribucion_Dependencia = new mongoose.Schema(
         codigo_presupuestal:{type:String, required: true,},
         presupuesto_inicial:{type:Number, required: true,},
         ano:{type:String, required: true,},
-        lote:{type:mongoose.Schema.Types.ObjectId,ref:'Lote', require:true},
+        dependecia:{type:mongoose.Schema.Types.ObjectId,ref:'Dependencia', require:true},
         items:{type:mongoose.Schema.Types.ObjectId,ref:'Items_presupuesto', require:true},
         createdAt: { type: Date, default: Date.now },
         status:{type:String,default:1}
