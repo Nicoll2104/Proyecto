85a9f937f1a9c9457613186c5d7b7126356f93a0
lauraRodrigue <>
2024-04-04 17:53:28 -0500

    lote

--- controllers/ficha.js
+++ controllers/ficha.js
@@ -1,10 +1,9 @@
 import ficha from "../models/ficha.js";
-import area_tematica from '../models/area_tematica.js'
 
 const httpFicha = {
     getFicha: async (req,res)=>{
         try {
-            const fichas = await ficha.find().populate('area_tematica');
+            const fichas = await ficha.find();
             res.json(fichas);
         } catch(error) {
             res.status(500).json({ error: 'Error interno del servidor' });
@@ -14,7 +13,7 @@ const httpFicha = {
     getFichaid: async (req,res)=>{
         const {id}= req.params
         try {
-            const fichas = await ficha.findById(id).populate('area_tematica');
+            const fichas = await ficha.findById(id);
             res.json({fichas})
         } catch(error) {
             res.status(400).json({error:'No se encontró el id'})
@@ -23,12 +22,9 @@ const httpFicha = {
 
     postFicha: async (req, res) => {
         try {
-            const { codigo_ficha, nombre, nivel_de_formacion, fecha_inicio, fecha_fin, area_tematica } = req.body;
-            const fichas = new ficha({ codigo_ficha, nombre, nivel_de_formacion, fecha_inicio, fecha_fin, area_tematica });
+            const { codigo, nombre, nivel_de_formacion, fecha_inicio, fecha_fin} = req.body;
+            const fichas = new ficha({ codigo, nombre, nivel_de_formacion, fecha_inicio, fecha_fin });
 
-            const rArea_tematica = await area_tematica.findById(area_tematica)
-
-            fichas.area = rArea_tematica
             await fichas.save(); 
             res.json({ mensaje: 'Ficha agregada con éxito', fichas });
         } catch (error) {
@@ -40,18 +36,15 @@ const httpFicha = {
     
     putFicha: async (req,res) =>{
         const {id} = req.params;
-        const {codigo_ficha, nombre, nivel_de_formacion, fecha_inicio, fecha_fin, area_tematica} = req.body;
+        const {codigo, nombre, nivel_de_formacion, fecha_inicio, fecha_fin} = req.body;
     
         try{
-            const fichas  = await ficha.findByIdAndUpdate(id, { codigo_ficha, nombre, nivel_de_formacion, fecha_inicio, fecha_fin, area_tematica}, { new: true });
+            const fichas  = await ficha.findByIdAndUpdate(id, { codigo, nombre, nivel_de_formacion, fecha_inicio, fecha_fin}, { new: true });
 
             if(!fichas){
                 return res.status(404).json({mensaje: 'La ficha no existe' })
             }
 
-            const rArea_tematica = await area_tematica.findById(area_tematica)
-
-            fichas.area = rArea_tematica
             res.json({ mensaje: 'Ficha actualizado con éxito', fichas });
         }catch(error){
             res.status(500).json({ error: 'Error interno del servidor' });
