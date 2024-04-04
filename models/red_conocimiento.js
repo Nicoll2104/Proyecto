9ff9df53d5843bb9432875d60995ca6e7a5f1d7d
sairarojas <131273447+YesidRojas06@users.noreply.github.com>
2024-04-04 17:15:16 -0500

    resignacion

--- models/red_conocimiento.js
+++ models/red_conocimiento.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const red_conocimiento = new mongoose.Schema(
     {
-        codigo:{type:String, required: true},
         nombre:{type:String, required: true,},
-        id_dependencia:{type:mongoose.Schema.Types.ObjectId,ref:'Dependencia',required:true},
         createdAt: { type: Date, default: Date.now },
         status:{type:String,default:1}
     })
