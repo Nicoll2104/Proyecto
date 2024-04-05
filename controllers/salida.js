d259af837c4174c5df2f842c332ea2a49d5202c6
sairarojas <131273447+YesidRojas06@users.noreply.github.com>
2024-04-05 17:17:22 -0500

    resignacion

--- controllers/salida.js
+++ controllers/salida.js
@@ -3,7 +3,7 @@ import salida from "../models/salida.js";
 const httpSalida = {
     getsalida: async (req, res) => {
         try {
-            const Salidas = await salida.find()
+            const Salidas = await salida.find().populate('idUsuario').populate('idPedido');
             res.json(Salidas);
         } catch (error) {
             console.error(error);
