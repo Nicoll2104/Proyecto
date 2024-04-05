d279a4611ed45d382c2eb9204bf409397001a3ee
Nicoll2104 <soda210461@gmail.com>
2024-04-05 14:49:09 -0500

    actual

--- routes/pedido.js
+++ routes/pedido.js
@@ -15,9 +15,10 @@ router.get("/pedido/:id",[
 router.post("/agregar",[
     check("fecha_creacion","La fecha de creacion es obligatoria").not().isEmpty(),
     check("fecha_entrega", "La fecha de entrega es obligatoria").not().isEmpty(),
+    check("completado", "El completado es obligatorio").not().isEmpty(),
     check("instructor_encargado", "El instructor encargado es obligatorio").not().isEmpty(),
-    check("ficha", "La ficha es obligatoria").not().isEmpty(),
-    check('rol_usuario', 'El usuario es obligatorio').not().isEmpty(),
+    check("destino", "El destino es obligatorio").not().isEmpty(),
+    check("total", "El total es obligatorio").not().isEmpty(),
     validarcampos
 ], httpPedido.postPedido);
 
