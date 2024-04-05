46da89f507955d2208b5e1b973ed2464d38ec311
lauraRodrigue <>
2024-04-05 15:46:33 -0500

    DisConLote

--- routes/dis_contrato_lote.js
+++ routes/dis_contrato_lote.js
@@ -1,46 +1,48 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import { validarcampos } from "../middlewares/validarcampos.js";
-import httpDistribucionFicha from "../controllers/distribucion_ficha.js";
+import httpDisContratoLote from "../controllers/dis_contrato_lote.js";
 
 const router = Router();
 
-router.get("/ver", httpDistribucionFicha.getDisFicha);
+router.get("/ver", httpDisContratoLote.getDisConL);
 
 router.get("/disFicha/:id",[
     check("id", "El id es obligatorio").not().isEmpty(),
     validarcampos
-], httpDistribucionFicha.getDisFichaId);
+], httpDisContratoLote.getDisConLId);
 
 router.post("/agregar",[
     check("codigo_auxiliar", "El codigo es obligatorio").not().isEmpty(),
-    check("presupuesto", "El presupuesto es obligatorio").not().isEmpty(),
-    check('distribucion_presupuesto', 'La distribucion_presupuesto es obligatoria').not().isEmpty(),
-    check("ficha", "La  ficha es obligatoria").not().isEmpty(),
+    check("presupuesto_asignado", "El presupuesto asignado es obligatorio").not().isEmpty(),
+    check('presupuesto_actual', 'El presupuesto actual es obligatorio').not().isEmpty(),
+    check("contrato", "El contrato es obligatorio").not().isEmpty(),
+    check("lote", "El lote es obligatorio").not().isEmpty(),
     validarcampos
-], httpDistribucionFicha.postDisFicha);
+], httpDisContratoLote.postDisConL);
 
 router.put("/modificar/:id",[
     check("codigo_auxiliar", "El codigo es obligatorio").not().isEmpty(),
-    check("presupuesto", "El presupuesto es obligatorio").not().isEmpty(),
-    check('distribucion_presupuesto', 'La distribucion_presupuesto es obligatoria').not().isEmpty(),
-    check("ficha", "La  ficha es obligatoria").not().isEmpty(),
+    check("presupuesto_asignado", "El presupuesto asignado es obligatorio").not().isEmpty(),
+    check('presupuesto_actual', 'El presupuesto actual es obligatorio').not().isEmpty(),
+    check("contrato", "El contrato es obligatorio").not().isEmpty(),
+    check("lote", "El lote es obligatorio").not().isEmpty(),
     validarcampos
-],httpDistribucionFicha.putDisFicha);
+],httpDisContratoLote.putDisConL);
 
 router.put("/inactivar/:id",[
     check("id", "Digite ID").not().isEmpty(),
     validarcampos
-], httpDistribucionFicha.putInactivar);
+], httpDisContratoLote.putInactivar);
 
 router.put("/activar/:id",[
     check("id", "Digite ID").not().isEmpty(),
     validarcampos
-], httpDistribucionFicha.putActivar);
+], httpDisContratoLote.putActivar);
 
 router.delete("/eliminar/:id",[
     check("id", "Digite ID").not().isEmpty(),
     validarcampos
-], httpDistribucionFicha.deleteDisFicha)
+], httpDisContratoLote.deleteDisConL)
 
 export default router;
