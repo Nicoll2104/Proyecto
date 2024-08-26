96951d3a367f86ae20f3cb8f75f380c36c448673
Saira Hidaly Velandia Rojas <saira.velandia@audifarma.com.co>
2024-08-26 09:11:11 -0500

    tabla en prueba

--- routes/items_presupuesto.js
+++ routes/items_presupuesto.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { validarcampos } from "../middlewares/validarcampos.js";
 import httpItemPresupuesto from "../controllers/items_presupuesto.js";
 
 const router = Router();
@@ -9,7 +8,7 @@ router.get("/ver", httpItemPresupuesto.getPresupuesto);
 
 router.get("/presupuesto/:id",[
     check("id", "El id es obligatorio").not().isEmpty(),
-    validarcampos
+
 ], httpItemPresupuesto.getPresupuestoId);
 
 router.post("/agregar",[
@@ -17,7 +16,6 @@ router.post("/agregar",[
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("presupuesto_inicial","El presupuesto inicial es obligatorio").not().isEmpty(),
     check("vigencia", "La vigencia es obligatoria").not().isEmpty(),
-    validarcampos
 ], httpItemPresupuesto.postPresupuesto);
 
 router.put("/modificar/:id",[
@@ -25,22 +23,18 @@ router.put("/modificar/:id",[
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("presupuesto_inicial","El presupuesto inicial es obligatorio").not().isEmpty(),
     check("vigencia", "La vigencia es obligatoria").not().isEmpty(),
-    validarcampos
 ],httpItemPresupuesto.putPresupuesto);
 
 router.put("/inactivar/:id",[
     check("id", "Digite ID").not().isEmpty(),
-    validarcampos
 ], httpItemPresupuesto.putInactivar);
 
 router.put("/activar/:id",[
     check("id", "Digite ID").not().isEmpty(),
-    validarcampos
 ], httpItemPresupuesto.putActivar);
 
 router.delete("/eliminar/:id",[
     check("id", "Digite ID").not().isEmpty(),
-    validarcampos
 ], httpItemPresupuesto.deletePresupuesto)
 
 export default router;
